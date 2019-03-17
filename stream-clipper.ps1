88d29912bc0c9ff59068dbee739a33aa6bb9b970
Ben Williams <bww1@iastate.edu>
2019-03-17 13:07:59 -0700

    Rendering text and concatenating fully

--- stream-clipper.ps1
+++ stream-clipper.ps1
@@ -6,6 +6,8 @@ function Get-FileName($initialDirectory)
 {
     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
     $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $openFileDialog.Title = "Select json-formatted config file."
+    $openFileDialog.Filter = "JSON (*.JSON)|*.JSON|All files (*.*)|*.*"
     $openFileDialog.ShowDialog() | Out-Null
     return $openFileDialog.FileName
 }
@@ -17,6 +19,7 @@ if (-not (Test-Path -Path $ConfigFile))
     if (-not (Test-Path -Path $ConfigFile))
     {
         Write-Error "Couldn't find $ConfigFile"
+        return
     }
 }
 
@@ -24,6 +27,7 @@ Write-Warning "This script tested with ffmpeg version 4.1.1"
 if (-not (Get-Command "ffmpeg.exe" -ErrorAction SilentlyContinue))
 {
     Write-Error "Can't find ffmpeg.  Make sure the latest release is installed and added to your Path.  If you just did this, you may have to refresh your powershell window."
+    return
 }
 
 Write-Output "Parsing $ConfigFile"
@@ -45,8 +49,8 @@ $outputFileBaseName = $($config.outputVideo).split('\.')[-2]
 $team1Score = 0
 $team2Score = 0
 
+# Create ffmpeg video splitting command
 $clipCommand = ""
-
 foreach ($clip in $config.clips)
 {
     Write-Output $clip
@@ -56,47 +60,67 @@ foreach ($clip in $config.clips)
     # Append clip to list
     $clipFiles += $clipFilePath
 
-    # Build clip command
-    $clipCommand += " -ss $($clip.start) -to $($clip.end) -c copy $clipFileName"
+    $clipCommand += " -ss $($clip.start) -to $($clip.end) -c copy ""$clipFilePath"""
     $clipCounter += 1
 }
 
-# Export clips at one time to speed this up
-ffmpeg -i $config.inputVideo $clipCommand -y
+# Export clips in one shot - massive speed up
+Invoke-Expression "ffmpeg -i ""$($config.inputVideo)"" $clipCommand -y"
 
-# Render text
 if ($config.showSeriesScore)
 {
-    foreach ($clipFile in $clipFiles)
+    # Track score for overlay
+    $scoreCounter = 0
+
+    # Store filenames for overlay
+    $clipOverlayFiles = @()
+
+    foreach ($clipFilePath in $clipFiles)
     {
         # Overlay score
         # We overlay with score - team name.  That's just to align easily.
         # I'll happily take a fix for team name - score with alignment :)
         $text = "$team1Score - $($config.team1)`r`n$team2Score - $($config.team2)"
         $textParams = "font=arial: text=`'$text`': fontsize=36: fontcolor=white: x=(w-text_w)/16: y=(h-text_h)/16"
-        ffmpeg -i $clipFile `
-            -vf drawtext=$textParams `
-            $clipFilePath -y 
 
-        if ($clip.winner -eq $config.team1)
+        # Create new clip file path
+        $clipBaseName = (Get-Item $clipFilePath).BaseName
+        $overlayBaseName = $clipBaseName + "_overlay"
+        $clipPath = Split-Path -Path $clipFilePath
+        $clipOverlayFilePath = Join-Path -Path $clipPath -ChildPath "$overlayBaseName.mp4"
+
+        # Overlay clip with text.  Running these in parrallel doesn't speed things up.
+        # ffmpeg does a good job of using all of the available CPU.
+        Invoke-Expression "ffmpeg -i ""$clipFilePath"" -vf drawtext=""$textParams"" ""$clipOverlayFilePath"" -y"
+
+        $clipOverlayFiles += $clipOverlayFilePath
+
+        $winner = $config.clips[$scoreCounter].winner
+        if ($winner -eq $config.team1)
         {
             $team1Score += 1
         }
-        elseif ($clip.winner -eq $config.team2)
+        elseif ($winner -eq $config.team2)
         {
             $team2Score += 1
         }
         else
         {
-            Write-Warning "Winner $($clip.winner) wasn't a recognized team."
+            Write-Warning "Winner $winner wasn't a recognized team."
         }
+
+        $scoreCounter += 1
     }
+
+    # Overwrite clip files
+    foreach ($clipFile in $clipFiles)
+    {
+        Remove-Item $clipFile -Force -Confirm:$false
+    }
+    $clipFiles = $clipOverlayFiles
 }
 
-$files = Get-ChildItem "$clipFolder\*" -Include *.mp4
-Write-Output $clipFiles
-
-# Pad games in the final video so video length doesn't give away 
+# Pad games in the final video so video length doesn't give away who won the series
 if ($config.padSeriesLength)
 {
     if ($clipCounter -lt $config.padSeriesLength)
@@ -104,16 +128,32 @@ if ($config.padSeriesLength)
         $clipPadCount = $config.padSeriesLength - $clipCounter
         Write-Output "Clip count of $clipCounter is < padSeriesLength of $($config.padSeriesLength)"
 
-        # Pad clip concat list
-        while ($clipPadCount -gt 0)
+        $clipPadIndex = 0
+        $firstPadClip = $clipFiles[$clipPadIndex]
+
+        # Create new clip file path
+        $clipBaseName = (Get-Item $firstPadClip).BaseName
+        $overlayBaseName = $clipBaseName + "_pad_overlay"
+        $clipPath = Split-Path -Path $clipFilePath
+        $clipOverlayFilePath = Join-Path -Path $clipPath -ChildPath "$overlayBaseName.mp4"
+
+        # Draw text explaining what's happening on the first padded clip
+        $text = "Padding the video to avoid giving away the winner"
+        $textParams = "enable='between(t,0,6)': font=arial: text=`'$text`': fontsize=36: fontcolor=white: x=(w-text_w)/2:y=(h-text_h)/2"
+        Invoke-Expression "ffmpeg -i ""$firstPadClip"" -vf drawtext=""$textParams"" ""$clipOverlayFilePath"" -y"
+
+        $clipFiles += $clipOverlayFilePath
+        $clipPadIndex += 1
+
+        # Pad clip concat list with unmodified videos
+        while ($clipPadIndex -gt $clipPadCount)
         {
-            $clipPadCount -= 1
-            $padClipFile = $clipFiles[$clipPadCount]
+            $padClipFile = $clipFiles[$clipPadIndex]
             $clipFiles += $padClipFile
             Write-Output "Padded $padClipFile"
+            $clipPadIndex += 1
         }
 
-        # Maybe drawtext explaining?
     }
     else
     {
@@ -121,18 +161,17 @@ if ($config.padSeriesLength)
     }
 }
 
+# Build a list of intermediate files to build the final video
 $concatList = ""
 foreach ($clipFile in $clipFiles)
 {
     $concatList += "file '$clipFile'`n"
 }
-
 $concatListFilePath = Join-Path -Path $clipFolder -ChildPath "concatListFile.txt"
 $concatList | Out-File $concatListFilePath -Force -Confirm:$false -Encoding ASCII
 
-# Perform concatenation
-ffmpeg -f concat -safe 0 -i $concatListFilePath `
-       -c copy $config.outputVideo -y
+# Perform video concatenation
+Invoke-Expression "ffmpeg -f concat -safe 0 -i ""$concatListFilePath"" -c copy ""$($config.outputVideo)"" -y"
 
 # Remove temporary files
 if (-not $config.saveIndividualClips)
@@ -142,7 +181,6 @@ if (-not $config.saveIndividualClips)
         Remove-Item $clipFile -Force -Confirm:$false
     }
 }
-
 Remove-Item $concatListFilePath
 
-Write-Host "Finished editing Rocket League"
+Write-Host "Finished creating $($config.outputVideo)"
